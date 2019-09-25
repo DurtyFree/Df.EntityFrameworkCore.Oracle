826dca3e9a56857da859d2a1b851b16dc219ebd1
Alexander Schmid <alexander.schmid.tdmsystems@outlook.com>
2019-09-25 15:16:46 +0200

    Fix ORA-00972: identifier is too long.
    Generates a random alias with a letter and a number between 0 to 9 to avoid the error below.

--- Df.EntityFrameworkCore.Oracle/Query/Internal/OracleQueryCompilationContext.cs
+++ Df.EntityFrameworkCore.Oracle/Query/Internal/OracleQueryCompilationContext.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using Df.EntityFrameworkCore.Oracle.Utilities;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
@@ -9,6 +11,8 @@ namespace Df.EntityFrameworkCore.Oracle.Query.Internal
 {
     public class OracleQueryCompilationContext : RelationalQueryCompilationContext
     {
+        private readonly ISet<string> _tableAliasSet = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
         public OracleQueryCompilationContext(
             [NotNull] QueryCompilationContextDependencies dependencies,
             [NotNull] ILinqOperatorProvider linqOperatorProvider,
@@ -23,5 +27,42 @@ namespace Df.EntityFrameworkCore.Oracle.Query.Internal
         }
 
         public override bool IsLateralJoinSupported => true;
+
+        public override int MaxTableAliasLength => 5;
+
+        private string GetRandomLetter(Random rng)
+        {
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int index = rng.Next(letters.Length);
+            return letters[index].ToString();
+        }
+
+        private string GetUniqueTableAlias()
+        {
+            var rdm = new Random();
+
+            return GetRandomLetter(rdm) + rdm.Next(0, 10);
+        }
+
+        public override string CreateUniqueTableAlias([NotNull] string currentAlias)
+        {
+            Check.NotNull(currentAlias, nameof(currentAlias));
+
+            if (currentAlias.Length == 0)
+            {
+                return currentAlias;
+            }
+
+            var uniqueAlias = GetUniqueTableAlias();
+
+            while (_tableAliasSet.Contains(uniqueAlias))
+            {
+                uniqueAlias = GetUniqueTableAlias();
+            }
+
+            _tableAliasSet.Add(uniqueAlias);
+
+            return uniqueAlias;
+        }
     }
 }
